34e457f4e8e238dc94a0d15950783cfe82db5074
LTVTech <johan@ltvtech.co.za>
2024-07-15 15:07:36 +0200

    Update environment.ts

--- angularfire-start/src/environments/environment.ts
+++ angularfire-start/src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
   firebase: {
-    apiKey: 'API_KEY',
-    authDomain: 'PROJECT_ID.firebaseapp.com',
-    databaseURL: 'https://PROJECT_ID.firebaseio.com',
-    projectId: 'PROJECT_ID',
-    storageBucket: 'PROJECT_ID.appspot.com',
-    messagingSenderId: 'SENDER_ID',
-    appId: 'APP_ID',
-    measurementId: 'G-MEASUREMENT_ID',
+    apiKey: 'AIzaSyD4tBkGgoB3szcjqc8fLLwJI1nI9sJ54iE',
+    authDomain: 'action-plan-h6uvvg.firebaseapp.com',
+    databaseURL: 'https://action-plan-h6uvvg-default-rtdb.europe-west1.firebasedatabase.app',
+    projectId: 'action-plan-h6uvvgD',
+    storageBucket: 'action-plan-h6uvvg.appspot.com',
+    messagingSenderId: '971104893551',
+    appId: '1:971104893551:web:04580a1cd0290b85ed6b9a',
+    measurementId: 'G-KXKC4D2B7D',
   },
 };
